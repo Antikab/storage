b5deb913cea8c09fb6077995a11f2a6fa11f236d
Arseny Prosvetov <arsprosvet@gmail.com>
2025-06-12 22:50:46 +0300

    добавлен лоадер

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <!-- App.vue -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFilesStore } from '@/stores/files'
+import LoaderOverlay from '@/components/LoaderOverlay.vue'
+
+const filesStore = useFilesStore()
+</script>
 
 <template>
   <div class="min-h-screen flex justify-center">
     <RouterView />
+    <LoaderOverlay v-if="filesStore.loading" />
   </div>
 </template>
