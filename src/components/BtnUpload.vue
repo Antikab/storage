2001c26664e47d917256321d163c8a6fd11b3f11
Arseny Prosvetov <arsprosvet@gmail.com>
2025-06-14 16:38:48 +0300

    Добавлено выпадающее меню

--- src/components/BtnUpload.vue
+++ src/components/BtnUpload.vue
@@ -4,7 +4,7 @@ import iconUpload from '@/assets/icons/icon-upload.svg'
 
 <template>
   <button
-    class="flex justify-center items-center gap-2 px-4 py-2.5 rounded-lg bg-primary-color hover:bg-primary-color-hover text-neutral-100 hover:text-white cursor-pointer transition-colors duration-300"
+    class="flex justify-center items-center gap-2 px-4 py-2.5 rounded-lg bg-primary-color hover:bg-primary-color-hover text-neutral-100 hover:text-white cursor-pointer transition-colors duration-250 select-none"
   >
     <inline-svg class="size-[20px]" :src="iconUpload" aria-label="icon btn upload" />
     <span class="text-sm font-medium">Upload</span>
--- src/components/FileRow.vue
+++ src/components/FileRow.vue
@@ -3,10 +3,11 @@
 import FileIcon from '@/components/FileIcon.vue'
 import { formatDate, formatSize, getFileIcon } from '@/utils/fileUtils'
 import { useFilesStore } from '@/stores/files'
-import { useFileDelete } from '@/composables/useFileDelete'
+// import { useFileDelete } from '@/composables/useFileDelete'
+import BtnMenu from '@/components/BtnMenu.vue'
 
 const filesStore = useFilesStore()
-const { deletingId, deleteFile } = useFileDelete()
+// const { deletingId, deleteFile } = useFileDelete()
 </script>
 
 <template>
@@ -22,10 +23,10 @@ const { deletingId, deleteFile } = useFileDelete()
     <div
       v-for="file in filesStore.files"
       :key="file.id"
-      class="flex flex-col lg:grid lg:grid-cols-[1fr_176px_176px_68px] items-start lg:items-center even:bg-light-gray-color-2 border-b-2 last:border-b lg:border-b border-b-light-gray-color hover:bg-primary-color-hover/5 active:bg-primary-color-hover/5 transition-colors duration-300"
+      class="flex flex-col lg:grid lg:grid-cols-[1fr_176px_176px_68px] items-start lg:items-center even:bg-light-gray-color-2 border-b-2 last:border-b lg:border-b border-b-light-gray-color hover:bg-primary-color-hover/5 active:bg-primary-color-hover/5 transition-colors duration-250"
     >
       <div
-        class="w-full lg:w-fit flex items-center gap-3 px-6 py-4 border-t border-t-light-gray-color lg:border-none shadow-xs lg:shadow-none mb-2 lg:m-0"
+        class="w-full lg:w-fit flex items-center gap-3 px-6 py-4 border-t border-t-light-gray-color lg:border-none shadow-xs lg:shadow-none lg:m-0"
       >
         <FileIcon :src="getFileIcon(file)" />
         <div class="flex flex-col">
@@ -36,18 +37,20 @@ const { deletingId, deleteFile } = useFileDelete()
         </div>
       </div>
 
-      <div class="w-full flex justify-between gap-2 lg:contents">
+      <div class="w-full flex items-center justify-between gap-2 lg:contents">
         <div class="flex flex-row-reverse lg:contents">
           <div class="flex flex-col lg:contents">
-            <div class="flex lg:hidden px-6 text-xs font-medium text-nowrap">File size</div>
+            <div class="flex lg:hidden px-6 pt-4 text-xs font-medium text-nowrap">File size</div>
             <div class="flex px-6 py-4 text-sm text-nowrap">{{ formatSize(file.size) }}</div>
           </div>
           <div class="flex flex-col sm:pl-13 lg:contents">
-            <div class="flex lg:hidden px-6 text-xs font-medium text-nowrap">Date uploaded</div>
+            <div class="flex lg:hidden px-6 pt-4 text-xs font-medium text-nowrap">
+              Date uploaded
+            </div>
             <div class="px-6 py-4 text-sm text-nowrap">{{ formatDate(file.date) }}</div>
           </div>
         </div>
-        <div class="flex flex-col items-start justify-center gap-2 text-sm pr-6 pb-4 lg:p-0">
+        <!-- <div class="flex flex-col items-start justify-center gap-2 text-sm pr-6 pb-4 lg:p-0">
           <a
             :href="file.url"
             target="_blank"
@@ -67,7 +70,8 @@ const { deletingId, deleteFile } = useFileDelete()
           >
             {{ deletingId === file.id ? 'Deleting...' : 'Delete' }}
           </button>
-        </div>
+        </div> -->
+        <BtnMenu :file="file" />
       </div>
     </div>
     <div v-if="!filesStore.files.length" class="px-6 py-4 text-center text-lg">
